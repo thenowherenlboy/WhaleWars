d45f81bb793a0d840e39b1f77d9f4e4b3f7186e6
thenowherenlboy <job_2614@hotmail.com>
2020-02-03 15:31:59 -0500

    Buidling some whales

--- WhaleWars/WhaleWars/Planet.cs
+++ WhaleWars/WhaleWars/Planet.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace WhaleWars
 {
-    class Planet
+    public class Planet
     {
     }
 }
--- WhaleWars/WhaleWars/Whales.cs
+++ WhaleWars/WhaleWars/Whales.cs
@@ -4,7 +4,47 @@ using System.Text;
 
 namespace WhaleWars
 {
-    class Whales
+    public class Whales
     {
+        protected int speed { get; set; }
+        protected int mass { get; set; }
+        protected int capacity { get; set; }
+
+        public Whales()
+        {
+            speed = 5;
+            mass = 500;
+            capacity = 25;
+        }
+
+        public Whales(int sp, int ms, int ca)
+        {
+            speed = sp;
+            mass = ms;
+            capacity = ca;
+        }
+
+        public void travel(Planet destination)
+        {
+            string dest = destination.ToString();
+            Console.WriteLine($"You are arrived at planet {dest}.");
+        }
     }
+
+    public class Orca : Whales
+    {
+        public Orca()
+        {
+            speed = 20;
+            mass = 250;
+            capacity = 5;
+        }
+
+        public void Attack(Object target)
+        {
+            // STUB
+           
+        }
+    }
+    
 }
