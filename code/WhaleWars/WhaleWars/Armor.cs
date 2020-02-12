47d67218fb61064042965d7f7861e9924129c6f3
Diego Marquez <59837903+diegomarquez-real@users.noreply.github.com>
2020-02-12 14:55:04 -0500

    armor class

---
+++ code/WhaleWars/WhaleWars/Armor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WhaleWars
+{
+    public class Armor : Item
+    {
+        CharClass _CharClass;
+        private int defenseModifier { get; set; }
+        public Armor(string name, int def, CharClass classClass)
+        {
+            Name = name;
+            _CharClass = classClass;
+            defenseModifier = def;           
+        }
+    }
+
+    public class Bronze : Armor
+    {
+        public Bronze();
+        {
+         Name = "Bronze Armor";
+         CharClass _CharClass = CharClass.fighter;
+        int defenseModifier = 1;
+         
+         
+        }
+    }
+
+}
